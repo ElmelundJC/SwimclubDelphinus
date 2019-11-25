ca8dcc93201decd849466d9a7f5acd3301878c72
JakobBundgaard <jakobbundgaard@hotmail.com>
2019-11-25 09:48:58 +0100

    25/11

--- Chairman.java
+++ Chairman.java
@@ -1,6 +1,18 @@
+import java.util.*;
+import java.io.*;
+
 public class Chairman extends Input{
 
-
+   Database data = new Database();
+   
+   Scanner scan = new Scanner(System.in);
+   
+   public void run() {
+       data.reader();
+   }
+   
+   
+   
    void Create(){
    
    }
